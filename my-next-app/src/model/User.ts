94485bdd2611605d2d54f1596c8d7760c3cfa001
chaitu0608 <chaitu0608@gmail.com>
2025-06-23 15:01:21 +0530

    zod schema set

--- my-next-app/src/model/User.ts
+++ my-next-app/src/model/User.ts
@@ -15,12 +15,57 @@ const MessageSchema: Schema<Message> = new Schema({
     required: true,
     default: Date.now,
   },
-
-  toJSON: {
-    transform: function (doc, ret) {
-      ret.id = ret._id;
-      delete ret._id;
-      delete ret.__v;
-    },
-  },
 });
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  isVerified: boolean;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isAcceptingMessages: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify Code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify Code Expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
