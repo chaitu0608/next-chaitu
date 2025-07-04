7b2734f3ef2ff3a2fae27fd803172c481c500f7b
chaitu0608 <chaitu0608@gmail.com>
2025-07-04 13:44:42 +0530

    Add next-auth dependency and create authentication route files

---
+++ my-next-app/src/app/api/test/sign-up/auth/[...nextauth]/options.ts
@@ -0,0 +1,19 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        username: { label: "Email", type: "text"},
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): {
+    }),
+  ],
+};
